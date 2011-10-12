48da4d792e7c16c92a79bece12bfedf0a08a207d
aceprahmat <acep.rahmat@reebonz.com>
2011-10-12 13:25:31 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jruby::Application.config.secret_token = '908b5a11253c38bc777cbbff5a3d4f8b2c51076f9ae2556800bdfd98186a2d6bd8c174610e3576d38d677e1f7bc41d30c60810d202636646148cf6ed86d93399'
